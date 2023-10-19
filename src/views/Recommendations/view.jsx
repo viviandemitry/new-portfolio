4f5920b50510466351658729e34b4e3691f835d3
Vivian De Mitry <vivian090@gmail.com>
2023-10-19 09:18:18 -0300

    testing responsive

--- src/views/Recommendations/view.jsx
+++ src/views/Recommendations/view.jsx
@@ -12,6 +12,11 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 
 
 export default function Recommendations() {
+
+  const { innerWidth } = window;
+
+  console.log(innerWidth)
+
   return (
     <Styles.Container>
       <Header />
@@ -19,6 +24,7 @@ export default function Recommendations() {
         <Styles.Title>
           RECOMMENDATIONS
         </Styles.Title>
+        { innerWidth >= 768 ?
         <Swiper
           cssMode={true}
           navigation={true}
@@ -57,6 +63,45 @@ export default function Recommendations() {
             </Styles.ContentContainer>
           </SwiperSlide>
         </Swiper>
+        : 
+        <Swiper
+        direction={'vertical'}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+        <Styles.ContentContainer>
+                <Styles.ImgContainer>
+                  <img src={Cesar} alt="Cesar_photo" />
+                  <Styles.ImgText>
+                    César Marcelo - IT Manager
+                  </Styles.ImgText>
+                </Styles.ImgContainer>
+                <Styles.TextContainer>
+                  <img src={Aspas} alt="Aspas_icon" /> <br />
+                  Vivian is an excellent person to work with. Eager to learn and an excellent developer. She's also a very good team player, always helping others and exposing her ideas.
+                  I really appreciated being her manager and I definitely recommend her.</Styles.TextContainer>
+              </Styles.ContentContainer>
+        </SwiperSlide>
+        <SwiperSlide>
+        <Styles.ContentContainer>
+              <Styles.ImgContainer>
+                  <img src={Fernanda} alt="Fernanda_photo" />
+                  <Styles.ImgText>
+                    Fernanda Caires - Project Manager
+                  </Styles.ImgText>
+                </Styles.ImgContainer>
+                <Styles.TextContainer>
+                  <img src={Aspas} alt="Aspas_icon" /> <br />
+                  Vivian is a very dedicated and committed professional. She learns everything very quickly and in a short time took over the management of projects in the area. She has great development potential and great interest in learning new things! Loves a challenge, and easily establishes trusting relationships
+                </Styles.TextContainer>
+            </Styles.ContentContainer>
+        </SwiperSlide>
+      </Swiper>
+     }
       </Styles.MainContainer>
     </Styles.Container>
   )
